68e39cd936ab5b77de8323446c2df436486e770d
Franco Dionofrio <dionofrio_franco@hotmail.com>
2025-10-15 17:20:09 -0300

    feat: implement function to fetch and display anime entries from JSON

---
+++ script.js
@@ -0,0 +1,25 @@
+const getAllAnimes = async ()  => {
+    const response = await fetch("./animes.json");
+    const animes =  response.json();
+    return animes
+}
+
+getAllAnimes().then(animes => {
+    const template = document.querySelector(".anime_container");
+    
+    animes.forEach(({ title, img, id }, i) => {
+        const card_anime = `
+                      <article class="card_anime">
+                          <a href="./description.html?id=${id}">
+                              <img width="164" height="255" src="${img}" alt="portada de ${title}">
+                              <div class="shadow"></div>
+                              <h3>${title}</h3>
+                          </a>
+                      </article>
+                  `;
+      
+        template.innerHTML += card_anime
+    })
+})
+
+
