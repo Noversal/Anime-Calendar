6c5bf3c2d24e73fd750e1accaf9db34ee4cd7f2d
Franco Dionofrio <dionofrio_franco@hotmail.com>
2025-10-17 23:34:45 -0300

    feat: add anime data files for completed and airing series, update script to render sections dynamically

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
-const getAllAnimes = async ()  => {
-    const response = await fetch("./animes.json");
-    const animes =  response.json();
-    return animes
+const getAllAnimes = async () => {
+  const response = await fetch("./animes.json");
+  const animes = response.json();
+  return animes;
+};
+
+const getAnimesByCategory = async ({ category }) => {
+  const response = await fetch(`./animes${category}.json`);
+  const animes = response.json();
+  return animes;
+};
+
+function createSection({ animes, category, limit = 4 }) {
+  const sectionAnimeContainer = document.createElement("section");
+  const headerSection = document.createElement("header");
+  headerSection.innerHTML = `
+    <h2>${category}</h2>
+    <a href="/categoria.html">Ver +</a>
+  `;
+  const containAnimes = document.createElement("div");
+
+  animes.forEach(({ title, img, id }, i) => {
+    if (i < limit) {
+      const card_anime = `
+                          <article class="card_anime">
+                              <a href="./pages/description?id=${id}">
+                                  <img width="230" height="370" src="${img}" alt="portada de ${title}">
+                                  <div class="shadow"></div>
+                                  <h3>${title}</h3>
+                              </a>
+                          </article>
+                      `;
+      containAnimes.innerHTML += card_anime;
+    }
+  });
+  sectionAnimeContainer.appendChild(headerSection);
+  sectionAnimeContainer.appendChild(containAnimes);
+
+  return sectionAnimeContainer;
 }
 
-getAllAnimes().then(animes => {
-    const template = document.querySelector(".anime_container");
-    
-    animes.forEach(({ title, img, id }, i) => {
-        const card_anime = `
-                      <article class="card_anime">
-                          <a href="./pages/description?id=${id}">
-                              <img width="164" height="255" src="${img}" alt="portada de ${title}">
-                              <div class="shadow"></div>
-                              <h3>${title}</h3>
-                          </a>
-                      </article>
-                  `;
-      
-        template.innerHTML += card_anime
-    })
-})
+const template = document.querySelector(".anime_container");
+const emsionAnimes = await getAnimesByCategory({ category: "Emision" });
+const completadoAnimes = await getAnimesByCategory({
+  category: "Completados",
+});
+const sectionEmision = createSection({
+  animes: emsionAnimes,
+  category: "Emision",
+});
+const sectionCompletado = createSection({
+  animes: completadoAnimes,
+  category: "Completado",
+});
 
-
+template.appendChild(sectionEmision);
+template.appendChild(sectionCompletado);
