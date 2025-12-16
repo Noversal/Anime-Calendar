c787be683e946465977d0d5c04e814c2a712fc9e
Franco Dionofrio <dionofrio_franco@hotmail.com>
2025-12-16 00:42:33 -0300

    feat: Included AnimeService for API integration (GET, POST, PUT, DELETE)

---
+++ Anime.js
@@ -0,0 +1,57 @@
+import { URL_API_BASE_PROD, URL_API_BASE_TEST } from "./const.js";
+
+export class AnimeService {
+    constructor(test = true) {
+        this.url = URL_API_BASE_PROD;
+        
+        if (!test){
+            this.url = URL_API_BASE_TEST;
+        }
+    }
+
+    async getAllAnimes() {
+        const response = await fetch(`${this.url}/animes`);
+        const data = await response.json();
+        return data;
+    }
+
+    async getAnimeById({ id }) {
+        const response = await fetch(`${this.url}/animes/${id}`);
+        const data = await response.json();
+        return data;
+    }
+
+    async createAnime(anime) {
+        const response = await fetch(`${this.url}/animes`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(anime),
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    async updateAnime({ id, anime }) {
+        const response = await fetch(`${this.url}/animes/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(anime),
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    async deleteAnime(id) {
+        const response = await fetch(`${this.url}/animes?id=${id}`, {
+            method: "DELETE",
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    
+}
