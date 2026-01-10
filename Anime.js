7d95d38df05b3ecd7581991c6d5828f4ec2ffae4
Franco Dionofrio <dionofrio_franco@hotmail.com>
2026-01-09 22:44:17 -0300

    feat: Enhance admin interface with multi-select genres, update functionality, and fallback image

--- Anime.js
+++ Anime.js
@@ -1,57 +1,62 @@
 import { URL_API_BASE_PROD, URL_API_BASE_TEST } from "./const.js";
 
 export class AnimeService {
-    constructor(test = true) {
-        this.url = URL_API_BASE_PROD;
-        
-        if (!test){
-            this.url = URL_API_BASE_TEST;
-        }
-    }
+	constructor(test = true) {
+		this.url = URL_API_BASE_PROD;
 
-    async getAllAnimes() {
-        const response = await fetch(`${this.url}/animes`);
-        const data = await response.json();
-        return data;
-    }
+		if (!test) {
+			this.url = URL_API_BASE_TEST;
+		}
+	}
 
-    async getAnimeById({ id }) {
-        const response = await fetch(`${this.url}/animes/${id}`);
-        const data = await response.json();
-        return data;
-    }
+	async getAllAnimes() {
+		const response = await fetch(`${this.url}/animes`);
+		const data = await response.json();
+		return data;
+	}
 
-    async createAnime(anime) {
-        const response = await fetch(`${this.url}/animes`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(anime),
-        });
-        const data = await response.json();
-        return data;
-    }
+	async getAnimeById({ id }) {
+		const response = await fetch(`${this.url}/animes/by_ids`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ids: id }),
+		});
 
-    async updateAnime({ id, anime }) {
-        const response = await fetch(`${this.url}/animes/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(anime),
-        });
-        const data = await response.json();
-        return data;
-    }
+		const data = await response.json();
+		return data;
+	}
 
-    async deleteAnime(id) {
-        const response = await fetch(`${this.url}/animes?id=${id}`, {
-            method: "DELETE",
-        });
-        const data = await response.json();
-        return data;
-    }
+	async createAnime(anime) {
+		const response = await fetch(`${this.url}/animes`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(anime),
+		});
+		const data = await response.json();
+		return data;
+	}
 
-    
+	async updateAnime({ id, anime = {} }) {
+		const response = await fetch(`${this.url}/animes/${id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(anime),
+		});
+		const data = await response.json();
+		return data;
+	}
+
+	async deleteAnime(id) {
+		const response = await fetch(`${this.url}/animes/${id}`, {
+			method: "DELETE",
+		});
+		const data = await response.json();
+		return data;
+	}
 }
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,19 +1,21 @@
 import { AnimeService } from "../../Anime.js";
 
-const animeService = new AnimeService(false);
+const animeService = new AnimeService();
 
 // DOM Elements
 const animeList = document.getElementById("animeList");
 
 const AdminCard = (anime) => {
+	const imageFallback = "../../public/fallback-image.png";
+
 	return `
 		<article class="admin-card">
-			<img src="${anime.img}" alt="${anime.title}">
+			<img src="${anime?.img || imageFallback}" alt="${anime.title}">
 			<div class="card-content">
 				<h3 class="card-title">${anime.title}</h3>
 				<p class="card-info">${anime.chapters} Episodios</p>
 				<div class="card-actions">
-					<button class="btn btn-edit" title="Editar">
+					<button class="btn btn-edit" data-id="${anime.id}" title="Editar">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 							<path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
 							<path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
@@ -30,32 +32,29 @@ const AdminCard = (anime) => {
 				</div>
 			</div>
 		</article>
-	`
-}
-
-
+	`;
+};
 
 // Render Functions
 function renderAnimes({ animes }) {
-	const listAnimes = animes
-		.map((anime) => AdminCard(anime))
-		.join("");
+	const listAnimes = animes.map((anime) => AdminCard(anime)).join("");
 
 	animeList.innerHTML = listAnimes;
 }
-
 
 animeService.getAllAnimes().then(({ animes }) => {
 	renderAnimes({ animes });
 });
 
+let oldAnimeData = {};
+
 // Anime List
-animeList.addEventListener("click", (e) => {
+animeList.addEventListener("click", async (e) => {
 	const animeTitle = document.getElementById("anime-title");
 	const animeCard = e.target.closest(".admin-card");
 	const animeCardTitle = animeCard.querySelector(".card-title").textContent;
 	animeTitle.textContent = animeCardTitle;
-	
+
 	const buttonDelete = e.target.closest(".btn-delete");
 	if (buttonDelete) {
 		const id = buttonDelete.dataset.id;
@@ -64,13 +63,92 @@ animeList.addEventListener("click", (e) => {
 		const confirmButton = dialogConfirm.querySelector("#confirm-delete-btn");
 		confirmButton.dataset.id = id;
 	}
+
+	const buttonEdit = e.target.closest(".btn-edit");
+	if (buttonEdit) {
+		const id = buttonEdit.dataset.id;
+		const { animes } = await animeService.getAnimeById({ id: [id] });
+		const { title, description, img, chapters, category } = animes[0]
+		oldAnimeData = { id, title, description, img, chapters, category }
+
+		const oldTitle = document.getElementById("old_title");
+		const oldDescription = document.getElementById("old_description");
+		const oldImage = document.getElementById("old_image");
+		const oldChapters = document.getElementById("old_chapters");
+
+		oldTitle.value = title;
+		oldDescription.value = description;
+		oldImage.value = img ?? "";
+		oldChapters.value = chapters;
+
+		// TODO: review pre-select genres and update counter
+		// Pre-select genres
+		const updateCheckboxes =
+			updateGenreContainer.querySelectorAll(".genre-checkbox");
+		updateCheckboxes.forEach((checkbox) => {
+			checkbox.checked =
+				category && category.includes(checkbox.value);
+		});
+
+		// Update counter
+		const selectedCount = updateGenreContainer.querySelectorAll(
+			".genre-checkbox:checked",
+		).length;
+		updateCounter.textContent = `(${selectedCount} seleccionados)`;
+
+		const dialogUpdate = document.getElementById("update-modal");
+		dialogUpdate.showModal();
+	}
 });
+
+// Selectors for Multi-select Genre (Add Modal)
+const addGenreContainer = document.getElementById("add-genresContainer");
+const addExpandBtn = document.getElementById("add-expandBtn");
+const addCollapseBtn = document.getElementById("add-collapseBtn");
+const addCounter = document.getElementById("add-selectedCounter");
+
+// Selectors for Multi-select Genre (Update Modal)
+const updateGenreContainer = document.getElementById("update-genresContainer");
+const updateExpandBtn = document.getElementById("update-expandBtn");
+const updateCollapseBtn = document.getElementById("update-collapseBtn");
+const updateCounter = document.getElementById("update-selectedCounter");
+
+// Helper function to handle expansion
+const setupGenreToggle = (container, expandBtn, collapseBtn) => {
+	expandBtn.addEventListener("click", () => {
+		container.classList.add("expanded");
+		expandBtn.hidden = true;
+		collapseBtn.hidden = false;
+	});
+	collapseBtn.addEventListener("click", () => {
+		container.classList.remove("expanded");
+		expandBtn.hidden = false;
+		collapseBtn.hidden = true;
+	});
+};
+
+// Helper function to update counter
+const setupCounter = (container, counterElement) => {
+	container.addEventListener("change", () => {
+		const selectedCount = container.querySelectorAll(
+			".genre-checkbox:checked",
+		).length;
+		counterElement.textContent = `(${selectedCount} seleccionados)`;
+	});
+};
+
+// add genre
+setupGenreToggle(addGenreContainer, addExpandBtn, addCollapseBtn);
+setupCounter(addGenreContainer, addCounter);
+// update genre
+setupGenreToggle(updateGenreContainer, updateExpandBtn, updateCollapseBtn);
+setupCounter(updateGenreContainer, updateCounter);
 
 // Delete Modal
 const modalActions = document.querySelector(".modal-actions");
 modalActions.addEventListener("click", async (e) => {
 	const animeTitle = document.getElementById("anime-title");
-	
+
 	const dialogConfirm = document.getElementById("delete-modal");
 
 	const buttonDelete = e.target.closest(".btn-delete");
@@ -79,6 +157,7 @@ modalActions.addEventListener("click", async (e) => {
 		await animeService.deleteAnime(id);
 		animeTitle.textContent = "";
 		dialogConfirm.close();
+		location.reload(); // Reload to see changes
 	}
 
 	const buttonCancel = e.target.closest(".btn-cancel");
@@ -88,7 +167,6 @@ modalActions.addEventListener("click", async (e) => {
 		animeTitle.textContent = "";
 	}
 });
-
 
 // Add Modal
 const addBtn = document.querySelector(".add-btn");
@@ -108,11 +186,18 @@ formAdd.addEventListener("submit", async (e) => {
 	const chapters = formData.get("chapters");
 	const img = formData.get("image");
 	const description = formData.get("description");
-	const genders = formData.get("gender");
+	const genders = formData.getAll("gender");
 
-	await animeService.createAnime({ title, chapters, img, description, genders });
+	await animeService.createAnime({
+		title,
+		chapters,
+		img,
+		description,
+		genders,
+	});
 	modalAdd.close();
 	e.target.reset();
+	addCounter.textContent = "(0 seleccionados)";
 });
 
 const btnCloseModal = document.querySelector(".btn-close-modal");
@@ -121,4 +206,72 @@ btnCloseModal.addEventListener("click", () => {
 	modalAdd.close();
 });
 
+const formUpdate = document.getElementById("update-form");
 
+formUpdate.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const formData = new FormData(e.target);
+	const title = formData.get("old_title");
+	const chapters = formData.get("old_chapters");
+	const img = formData.get("old_image").trim();
+	const description = formData.get("old_description");
+
+	// Collect all selected genres
+	const selectedGenders = formData.getAll("old_gender");
+
+	const newAnime = {};
+
+	if (title !== oldAnimeData.title) {
+		newAnime.title = title;
+	}
+
+	if (Number(chapters) !== oldAnimeData.chapters) {
+		newAnime.chapters = chapters;
+	}
+
+	if (img && img !== oldAnimeData.img) {
+		newAnime.img = img;
+	}
+
+	if (description !== oldAnimeData.description) {
+		newAnime.description = description;
+	}
+
+	// Compare categories (array comparison)
+	const oldCategories = oldAnimeData.category || [];
+
+	const lengthChanged = selectedGenders.length !== oldCategories.length;
+	const contentChanged = !selectedGenders.every((gender) =>
+		oldCategories.includes(gender),
+	);
+	const categoriesChanged = lengthChanged || contentChanged;
+
+	if (categoriesChanged) {
+		newAnime.category = selectedGenders;
+	}
+
+	try {
+		const response = await animeService.updateAnime({
+			id: oldAnimeData.id,
+			anime: newAnime,
+		});
+		console.log(response.status);
+		return;
+	} catch (error) {
+		console.error(error);
+	}
+
+	const dialogUpdate = document.getElementById("update-modal");
+	dialogUpdate.close();
+	e.target.reset();
+	updateCounter.textContent = "(0 seleccionados)";
+});
+
+const btnCloseUpdateModal = document.querySelector(
+	"#update-modal .btn-close-modal",
+);
+
+btnCloseUpdateModal.addEventListener("click", () => {
+	const dialogUpdate = document.getElementById("update-modal");
+	dialogUpdate.close();
+});
