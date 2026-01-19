7daef6ce8b10fc5988eadbf7fd55d6fd7bc99596
Franco Dionofrio <dionofrio_franco@hotmail.com>
2026-01-19 20:43:46 -0300

    feat: improve empty/error states UI with custom chibi assets and theme-consistent loader

--- script.js
+++ script.js
@@ -47,19 +47,50 @@ async function renderStateSection({ stateID }) {
 			return anime.id;
 		}
 		return null;
-	});
+	}).filter(Boolean);
 
-	const response = await fetch(`${URL_API_BASE_PROD}/animes/by_ids`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ ids: animeSelected }),
-	});
+	template.innerHTML = "";
+	const statusContainer = document.createElement("div");
+	statusContainer.classList.add("container-status");
+	const loaderElement = document.createElement("span");
+	loaderElement.classList.add("loader");	
+	statusContainer.appendChild(loaderElement);
+	template.appendChild(statusContainer);
+
+	if (animeSelected.length === 0) {
+		statusContainer.innerHTML = `
+		<img src="public/empty_state.png" width="300" alt="No hay animes" class="empty_state">
+		<p>Como que no estas viendo nada aun</p>
+		<a href="pages/catalog" class="add-anime">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+				<path d="M3.3335 8H12.6668" stroke="currentcolor" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"></path>
+				<path d="M8 3.3335V12.6668" stroke="currentcolor" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"></path>
+			</svg>
+			<span>Agregar Animes</span>
+		</a>`;
+		return;
+	}
+
+	let response;
+	
+	try {
+		response = await fetch(`${URL_API_BASE_PROD}/animes/by_ids`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ids: animeSelected }),
+		});
+	}
+	catch (error) {
+		statusContainer.innerHTML = `
+			<img src="public/error_state.png" width="300" alt="Error al cargar" class="error_state">
+			<p>Error al cargar los animes</p>
+		`;
+		return;
+	}
 
 	const { animes } = await response.json();
-	console.log({ animes });
-
 	const sectionEmision = createSection({
 		animes,
 	});
